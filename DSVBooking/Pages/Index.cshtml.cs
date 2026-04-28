69549dc379816ea823f2faeaf91e05277f2d6d17
Victoria Bruhn <vib006@edu.zealand.dk>
2026-04-28 12:09:15 +0200

    index.cshtml og index.cshtml.cs

--- DSVBooking/Pages/Index.cshtml.cs
+++ DSVBooking/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using DSVBooking.Models;
+using DSVBooking.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +7,33 @@ namespace DSVBooking.Pages
 {
     public class IndexModel : PageModel
     {
+        //Tom liste der fyldes i OnGet(). = new List<>() sørger for at der altid er en tom liste, hvis fx. onGet() ikke fylder listen ud
+        public List<MeetingRoom> Rooms { get; set; } = new List<MeetingRoom>();
+
+        //BindProperty = Indstruks til ASP.NET, når en bruger submitter en form, find den form field der matcher og put værdierne derind
+        //Da vores filter form bruger method="get" fortæller vi også ASP.NET til at bind når vi har GET requests med SupportsGet = true
+        [BindProperty(SupportsGet = true)]
+        public int MinCapacity { get; set; } = 0;
+
+        [BindProperty(SupportsGet = true)]
+
+        public string SelectedEquipment { get; set; } = "";
+
+        //Kører når siden indlæses. Checker hvilke filtre er aktive og finder matchende lokaler fra repository
         public void OnGet()
         {
-
+            if (SelectedEquipment != "" && SelectedEquipment != null)
+            {
+                Rooms = MeetingRoomRepository.GetByEquipment(SelectedEquipment);
+            }
+            else if (MinCapacity > 0)
+            {
+                Rooms = MeetingRoomRepository.GetByMinCapacity(MinCapacity);
+            }
+            else
+            {
+                Rooms = MeetingRoomRepository.GetAll();
+            }
         }
     }
 }
